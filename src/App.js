a829a95d48f1c82481d938283ca4def3ea337d47
Olga Skrypets <114450143+uhpoler@users.noreply.github.com>
2024-08-17 14:52:18 +0300

    Updated handleSelection

--- src/App.js
+++ src/App.js
@@ -44,7 +44,6 @@ export default function App() {
   }
 
   function handleSelection(friend) {
-    // setSelectedFriend(friend);
     setSelectedFriend((cur) => (cur?.id === friend?.id ? null : friend));
     setShowAddFriend(false);
   }
